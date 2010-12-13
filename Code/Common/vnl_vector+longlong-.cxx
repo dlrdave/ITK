671b51f07625f0eeac5be71aa7f6531ecc9cae9c
David Cole <david.cole@kitware.com>
2010-12-13 13:46:48 -0500

    ENH: Change size, index and offset definitions: use size_t and ptrdiff_t
    This commit makes the changes in the Numerics directory.
    
    At this point, this code compiles and links for me on Win64.
    (with 23 test failures now out of 1835)
    
    Change-Id: I5dfd8baa0205b6139f5938aa58170c6cca09c5e2

---
+++ Code/Common/vnl_vector+longlong-.cxx
@@ -0,0 +1,48 @@
+/*=========================================================================
+ *
+ *  Copyright Insight Software Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifdef _WIN64
+#include "vnl/vnl_vector.txx"
+
+#ifdef _WIN64
+VCL_DEFINE_SPECIALIZATION
+class vnl_numeric_traits<long long>
+{
+ public:
+  //: Additive identity
+  static const long long zero VCL_STATIC_CONST_INIT_INT_DECL(0);
+  //: Multiplicative identity
+  static const long long one VCL_STATIC_CONST_INIT_INT_DECL(1);
+  //: Maximum value which this type can assume
+  static const long long maxval; // = 0x7fffffff;
+  //: Return value of abs()
+  typedef unsigned long long abs_t;
+  //: Name of a type twice as long as this one for accumulators and products.
+  typedef long long double_t;
+  //: Name of type which results from multiplying this type with a double
+  typedef double real_t;
+};
+
+#if !VCL_CANNOT_SPECIALIZE_CV
+VCL_DEFINE_SPECIALIZATION
+class vnl_numeric_traits<long long const> : public vnl_numeric_traits<long long> {};
+#endif
+
+#endif   // _WIN64
+
+VNL_VECTOR_INSTANTIATE(long long);
+#endif
