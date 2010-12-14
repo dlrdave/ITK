1a89f22d487e4297959d6b3ef2317c3471fd9572
David Cole <david.cole@kitware.com>
2010-12-14 06:19:02 -0500

    ENH: Move vnl code into vnl. Fix warnings.
    Change-Id: I7669eca515d67bf95922d504bd2a24055f807b02

--- Code/Common/vnl_c_vector+longlong-.cxx
+++ Code/Common/vnl_c_vector+longlong-.cxx
@@ -31,31 +31,5 @@
 VCL_DEFINE_SPECIALIZATION_MACRO(long long);
 #undef VCL_DEFINE_SPECIALIZATION_MACRO
 
-#ifdef _WIN64
-VCL_DEFINE_SPECIALIZATION
-class vnl_numeric_traits<long long>
-{
- public:
-  //: Additive identity
-  static const long long zero VCL_STATIC_CONST_INIT_INT_DECL(0);
-  //: Multiplicative identity
-  static const long long one VCL_STATIC_CONST_INIT_INT_DECL(1);
-  //: Maximum value which this type can assume
-  static const long long maxval; // = 0x7fffffff;
-  //: Return value of abs()
-  typedef unsigned long long abs_t;
-  //: Name of a type twice as long as this one for accumulators and products.
-  typedef long long double_t;
-  //: Name of type which results from multiplying this type with a double
-  typedef double real_t;
-};
-
-#if !VCL_CANNOT_SPECIALIZE_CV
-VCL_DEFINE_SPECIALIZATION
-class vnl_numeric_traits<long long const> : public vnl_numeric_traits<long long> {};
-#endif
-
-#endif   // _WIN64
-
 VNL_C_VECTOR_INSTANTIATE_ordered(long long);
 #endif
